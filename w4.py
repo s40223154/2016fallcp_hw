0ba8df053d8590f9f30916ceb1096bc3dca2a36c
s40223154 <40223154@gm.nfu.edu.tw>
2016-09-30 15:14:19 +0800

    Updated from Brython Server: 2016/9/30 下午3:14:20

--- w4.py
+++ w4.py
@@ -4,5 +4,5 @@ print(mystring)
 '''
 prompt  = "Enter a some text "
 thetext = input(prompt)
-print ("This is what you entered:","test"sep="---")
+print ("This is what you entered:","test"sep="ppap")
 print (thetext)
